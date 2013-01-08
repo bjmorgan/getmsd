623dfcb513b89f99d7fbdda407d40d210f1cc33e
Ben Morgan <benmorgan2@gmail.com>
2013-01-08 15:56:50 +0000

    bash script for msd analysis

---
+++ regression.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python
+
+import sys
+from string import split
+
+x = []
+y = []
+xx = []
+xy = []
+
+while 1:
+	thisline = sys.stdin.readline()
+	if thisline != '':
+		data = split(thisline)
+		x.append(float(data[0]))
+		y.append(float(data[1]))
+	else:
+		sys.stdout.flush()
+		break
+
+n = len(x)
+for i in range(n):
+	xx.append(x[i]*x[i])
+	xy.append(x[i]*y[i])
+
+sx = sum(x)
+sy = sum(y)
+sxx = sum(xx)
+sxy = sum(xy)
+
+slope = (n*sxy - sx*sy) / (n*sxx - sx*sx)
+intercept = (sy - slope*sx)/n
+
+print intercept, slope
+
